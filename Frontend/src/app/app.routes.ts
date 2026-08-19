fa241a342a12c45257eb179e6c553cde2c60b504
Carlos-Vital <carlosknn2020@gmail.com>
2026-08-19 20:51:27 -0300

    feature: adicionado pagina inicial

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ClientHomepage } from './features/client/homepage/client-homepage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ClientHomepage
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ Frontend/src/app/features/client/homepage/client-homepage.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-homepage',
+  templateUrl: './client-homepage.html',
+  styleUrl: './client-homepage.css'
+})
+export class ClientHomepage {}
