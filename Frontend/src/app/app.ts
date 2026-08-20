19aaff511d670fabd7146bc4544c7d62750a33fd
Mateus Gabriel <mateusgpk1@gmail.com>
2026-08-20 20:35:42 -0300

    campos Cadastro

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
+import { CadastroComponent } from './features/Autocadastro/pages/cadastro';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, CadastroComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
