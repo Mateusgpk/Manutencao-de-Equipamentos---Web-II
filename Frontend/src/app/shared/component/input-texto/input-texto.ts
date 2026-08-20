4aa97a95e9f89a50caca448080c20610543775f8
Nicolas Mendes Fernandes <nicolasmendes512@gmail.com>
2026-08-20 11:45:51 -0300

    Adicionando componente reutilizável de formulário de Input de Texto

--- Frontend/src/app/shared/component/input-texto/input-texto.ts
+++ Frontend/src/app/shared/component/input-texto/input-texto.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-texto',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './input-texto.html',
-  styleUrl: './input-texto.css',
+  styleUrl: './input-texto.css'
 })
-export class InputTexto {}
+
+
+export class InputTexto {
+  @Input({ required: true }) label!: string;
+  @Input({ required: true }) control!: FormControl;
+
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() id: string = '';
+}
