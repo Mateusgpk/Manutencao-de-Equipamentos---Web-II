022b83ba61790ebc43f78107bdbb8b02336f8f7d
joaomanoelvalerio <joaomanoelval@gmail.com>
2026-08-19 22:34:27 -0300

    Squash merge profileEmployee into main

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
 import { ClientHomepage } from './features/client/homepage/client-homepage';
+import { EmployeeHomepage } from './features/employee/homepage/employee-homepage';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ClientHomepage
+    component: ClientHomepage,
+  },
+  {
+    path: 'employee/home',
+    component: EmployeeHomepage,
   },
   {
     path: '**',
-    redirectTo: ''
-  }
+    redirectTo: '',
+  },
 ];
---
+++ Frontend/src/app/features/employee/homepage/employee-homepage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  imports: [],
+  templateUrl: './employee-homepage.html',
+  styleUrl: './employee-homepage.css',
+})
+export class EmployeeHomepage {}
