1b9687ef06c63e41ba9a32f3fb21d1b3269837b8
Nicolas Mendes Fernandes <nicolasmendes512@gmail.com>
2026-08-20 11:45:12 -0300

    Utilizando componentes reutilizáveis de formulário em Solicitar Manutenção

--- Frontend/src/app/features/client/solicitar-manutencao/solicitar-manutencao.ts
+++ Frontend/src/app/features/client/solicitar-manutencao/solicitar-manutencao.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { InputTexto } from '../../../shared/component/input-texto/input-texto';
 
 @Component({
   selector: 'app-solicitar-manutencao',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, InputTexto], 
   templateUrl: './solicitar-manutencao.html',
   styleUrl: './solicitar-manutencao.css',
 })
-export class SolicitarManutencao {}
+export class SolicitarManutencao {
+
+  formSolicitacao = new FormGroup({
+    descricaoEquipamento: new FormControl(''),
+    categoriaEquipamento: new FormControl('')
+  });
+
+}
