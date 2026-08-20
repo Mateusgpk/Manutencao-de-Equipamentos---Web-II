b1f8bf39d5cfff25c0b57c8efcdf7dfae5da4511
Nicolas Mendes Fernandes <nicolasmendes512@gmail.com>
2026-08-20 11:34:15 -0300

    Criando componente padrão de input de texto para formulários

---
+++ Frontend/src/app/shared/component/input-texto/input-texto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-texto',
+  imports: [],
+  templateUrl: './input-texto.html',
+  styleUrl: './input-texto.css',
+})
+export class InputTexto {}
