0c033b32e99eb708d3bb61045e5b40768522a371
Nicolas Mendes Fernandes <nicolasmendes512@gmail.com>
2026-08-27 19:40:04 -0300

    Adicionando campo de Descrição do Problema ao formulário de Solicitar manutenção via componente

--- Frontend/src/app/features/client/solicitar-manutencao/solicitar-manutencao.ts
+++ Frontend/src/app/features/client/solicitar-manutencao/solicitar-manutencao.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { InputTexto } from '../../../shared/component/input-texto/input-texto';
+import { TextArea } from '../../../shared/component/text-area/text-area';
 
 @Component({
   selector: 'app-solicitar-manutencao',
   standalone: true,
-  imports: [ReactiveFormsModule, InputTexto], 
+  imports: [ReactiveFormsModule, InputTexto, TextArea],
   templateUrl: './solicitar-manutencao.html',
   styleUrl: './solicitar-manutencao.css',
 })
@@ -13,7 +14,8 @@ export class SolicitarManutencao {
 
   formSolicitacao = new FormGroup({
     descricaoEquipamento: new FormControl(''),
-    categoriaEquipamento: new FormControl('')
+    categoriaEquipamento: new FormControl(''),
+    descricaoProblema: new FormControl(''),
   });
 
 }
