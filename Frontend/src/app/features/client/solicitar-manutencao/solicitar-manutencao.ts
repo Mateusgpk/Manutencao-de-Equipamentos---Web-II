7984b5df348d1d0e933500bc4050f4a26c40ebd4
Nicolas Mendes Fernandes <nicolasmendes512@gmail.com>
2026-09-03 10:53:00 -0300

    Integrando solicitar-manutencao com o Service responsável

--- Frontend/src/app/features/client/solicitar-manutencao/solicitar-manutencao.ts
+++ Frontend/src/app/features/client/solicitar-manutencao/solicitar-manutencao.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { InputTexto } from '../../../shared/component/input-texto/input-texto';
 import { TextArea } from '../../../shared/component/text-area/text-area';
+import { SolicitacaoService } from '../services/solicitacao';
 
 @Component({
   selector: 'app-solicitar-manutencao',
@@ -11,7 +12,7 @@ import { TextArea } from '../../../shared/component/text-area/text-area';
   styleUrl: './solicitar-manutencao.css',
 })
 export class SolicitarManutencao {
-  
+
   private formBuilder = inject(FormBuilder);
 
   formSolicitacao = this.formBuilder.nonNullable.group({
@@ -20,12 +21,21 @@ export class SolicitarManutencao {
     descricaoProblema: ['', [Validators.required, Validators.minLength(10)]]
   });
 
+  private solicitacaoService = inject(SolicitacaoService);
+
   enviarSolicitacao() {
     if (this.formSolicitacao.valid) {
       const payload = this.formSolicitacao.getRawValue();
-      console.log('Dados Validados com Sucesso!', payload);
+
+      this.solicitacaoService.enviarNovaSolicitacao(payload).subscribe({
+        next: (resposta) => {
+          console.log('Enviado com sucesso!', resposta);
+        },
+        error: (erro) => {
+          console.error('Falha ao enviar:', erro);
+        }
+      });
     } else {
-      console.warn("Formulário Inválido, revise os Dados Informados");
       this.formSolicitacao.markAllAsTouched();
     }
   }
