ff3bafa394e8317819fb76c25e413703dd9f132b
Nicolas Mendes Fernandes <nicolasmendes512@gmail.com>
2026-09-24 23:39:22 -0300

    Implementando select de categorias de equipamentos na tela de solicitação de manuntenção com opções predefinidas

--- Frontend/src/app/features/cliente/pages/solicitar-manutencao/SolicitarManutencao.ts
+++ Frontend/src/app/features/cliente/pages/solicitar-manutencao/SolicitarManutencao.ts
@@ -1,17 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+
 import { InputTexto } from '../../../../shared/components/input-texto/input-texto';
 import { TextArea } from '../../../../shared/components/text-area/text-area';
+import { Select } from '../../../../shared/components/select/select';
+
+import { SelectOption } from '../../../../shared/models/select.model';
+
 import { SolicitacaoService } from '../../services/solicitacao';
 
 @Component({
   selector: 'app-solicitar-manutencao',
   standalone: true,
-  imports: [ReactiveFormsModule, InputTexto, TextArea],
+  imports: [ReactiveFormsModule, InputTexto, TextArea, Select],
   templateUrl: './SolicitarManutencao.html',
   styleUrl: './SolicitarManutencao.css',
 })
 export class SolicitarManutencao {
+
+  opcoesCategoriaEquipamento: SelectOption[] = [
+    { value: 'desktop', label: 'Desktop' },
+    { value: 'notebook', label: 'Notebook' },
+    { value: 'celular', label: 'Celular' },
+    { value: 'tablet', label: 'Tablet' },
+    { value: 'impressora', label: 'Impressora' },
+    { value: 'outros', label: 'Outros' }
+  ];
 
   private formBuilder = inject(FormBuilder);
 
